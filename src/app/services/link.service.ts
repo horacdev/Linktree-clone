5cee87a7888b16be8b37141118998ba18f6046ec
Horacio <horaciodev7@gmail.com>
2025-03-26 22:44:29 -0300

    Adicionando `curriculo`

--- src/app/services/link.service.ts
+++ src/app/services/link.service.ts
@@ -8,7 +8,7 @@ export class LinkService {
   private links: Link[] = [
     { title: 'GitHub', url: 'https://github.com/horacdev', icon: 'fab fa-github' },
     { title: 'LinkedIn', url: 'https://www.linkedin.com/in/hor%C3%A1cio-paganini-8149b7328', icon: 'fab fa-linkedin' },
-    
+    {title: 'Meu Currículo', url: 'assets/docs/curriculo.pdf', icon: 'fas fa-file-pdf'}
   ];
   getLinks(): Link[] {
     return this.links;
