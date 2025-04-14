825ebb67b623237134164d93bd97da436e5f4014
Horacio <horaciodev7@gmail.com>
2025-04-14 17:10:10 -0300

    estilização

--- src/app/services/link.service.ts
+++ src/app/services/link.service.ts
@@ -8,7 +8,8 @@ export class LinkService {
   private links: Link[] = [
     { title: 'GitHub', url: 'https://github.com/horacdev', icon: 'fab fa-github' },
     { title: 'LinkedIn', url: 'https://www.linkedin.com/in/hor%C3%A1cio-paganini-8149b7328', icon: 'fab fa-linkedin' },
-    {title: 'Meu Currículo', url: 'assets/docs/curriculo.pdf', icon: 'fas fa-file-pdf'}
+    { title: 'Meu Currículo', url: 'assets/docs/curriculo.pdf', icon: 'fas fa-file-pdf'},
+    { title: 'Email', url: 'mailto:horaciodev7@gmail.com', icon: 'fas fa-envelope'}
   ];
   getLinks(): Link[] {
     return this.links;
